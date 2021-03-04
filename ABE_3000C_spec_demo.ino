a4b123796f35953c317dacba83631696a2290feb
J. Barrett Carter <72092594+barrettcarter@users.noreply.github.com>
2021-03-04 14:54:42 -0500

    Update ABE_3000C_spec_demo.ino
    Added code for OLED display.

--- ABE_3000C_spec_demo.ino
+++ ABE_3000C_spec_demo.ino
@@ -1,12 +1,38 @@
 #include "AS726X.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 AS726X sensor;
 
 void setup() {
   Wire.begin();
-  Serial.begin(9600);
 
   sensor.begin();
+
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
 }
 
 void loop() {
@@ -51,4 +77,18 @@ void loop() {
   Serial.print("]");
 
   Serial.println();
+
+  //power_red = sensor.getCalibratedR();
+  
+  display.clearDisplay();
+
+  display.setTextSize(2);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("P_Red:"));
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE); // Draw 'inverse' text
+  display.println(sensor.getCalibratedRed());
+  display.display();
+    
 }
